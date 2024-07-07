50c1241c20147278c9c8fa1b5af6684d51b115b7
Niels Peen <niels.peen@wanwire.com>
2024-07-07 18:27:10 -0500

    a

--- resources/views/partials/sidebar_menu_toggle.blade.php
+++ resources/views/partials/sidebar_menu_toggle.blade.php
@@ -1,4 +1,4 @@
-<button type="button" class="sidebar-menu-toggle" data-toggle="tooltip" data-placement="bottom" title="{{ __('Open Submenu') }}" aria-expanded="false">
+<button type="button" class="bg-yellow-200 border-amber-300 border-2 sidebar-menu-toggle" data-toggle="tooltip" data-placement="bottom" title="{{ __('Open Submenu') }}" aria-expanded="false">
     <span class="sr-only">{{ __('Toggle Navigation') }}</span>
     <span class="icon-bar"></span>
     <span class="icon-bar"></span>
---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import {defineConfig} from 'vite';
+import laravel, {refreshPaths} from 'laravel-vite-plugin';
+
+export default defineConfig({
+    build: {
+        assetsDir: 'final',
+    },
+    plugins: [
+        laravel({
+            input:
+                [
+                    'resources/sass/app.scss',
+                    'resources/js/app.js'
+                    // 'resources/css/filament.css'
+                ],
+            refresh:
+                [
+                    ...refreshPaths,
+                    'app/Livewire/**',
+                    'app/Filament/**'
+                ]
+        }),
+    ],
+    server: {
+        https: {
+            key: "/Users/niels/.config/valet/Certificates/freescout.test.key",
+            cert:"/Users/niels/.config/valet/Certificates/freescout.test.crt",
+        },
+        host: 'freescout.test',
+        hmr: {
+            host: 'freescout.test',
+        },
+    },
+})
+;
